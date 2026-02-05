bfaeae7c724dacf6a33e006a3e8d05efcd82b862
Roy Ashbrook <royashbrook@users.noreply.github.com>
2026-02-05 13:56:37 -0500

    Add back Safety Page

--- src/Content.svelte
+++ src/Content.svelte
@@ -1,8 +1,10 @@
 <script>
   import Router from "svelte-spa-router";
   import Billing from "./routes/Billing/Billing.svelte";
+  import Safety from "./routes/Safety/Safety.svelte";
   const routes = {
     "/": Billing,
+    "/Safety": Safety,
   };
 </script>
 
---
+++ src/routes/Safety/Safety.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Safety from './kpi.svelte'
+  import CoachedChart from './CoachedChart.svelte'
+  import DaysToCoachChart from './DaysToCoachChart.svelte'
+  import SafetyScoreChart from './SafetyScoreChart.svelte'
+</script>
+
+<Safety />
+<CoachedChart />
+<DaysToCoachChart />
+<SafetyScoreChart />
